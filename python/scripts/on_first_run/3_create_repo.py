2d6d5dad4378e05916458c33e07ea31e5ce56ff5
LEOGLLRD <92512809+LEOGLLRD@users.noreply.github.com>
2025-03-24 15:32:39 +0100

    Update 3_create_repo.py
    Added the shacl validation to the created repositories

--- python/scripts/on_first_run/3_create_repo.py
+++ python/scripts/on_first_run/3_create_repo.py
@@ -37,42 +37,60 @@ if config.getboolean("USERS", "create_users_with_pattern"):
     for i in range(1, number_of_users + 1):
         username_pattern = new_users_pattern.replace("#", str(i).zfill(2))
         ttl_data = f"""
-            @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-            @prefix rep: <http://www.openrdf.org/config/repository#> .
-            @prefix sr: <http://www.openrdf.org/config/repository/sail#> .
-            @prefix sail: <http://www.openrdf.org/config/sail#> .
+            @prefix config: <tag:rdf4j.org,2023:config/> .
+                    @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+                    @prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
 
-            <#{username_pattern}> a rep:Repository;
-                rep:repositoryID "{username_pattern}";
-                rep:repositoryImpl [
-                    rep:repositoryType "graphdb:SailRepository";
-                    <http://www.openrdf.org/config/repository/sail#sailImpl> [
-                        <http://www.ontotext.com/config/graphdb#base-URL> "http://example.org/owlim#";
-                        <http://www.ontotext.com/config/graphdb#check-for-inconsistencies> "false";
-                        <http://www.ontotext.com/config/graphdb#defaultNS> "";
-                        <http://www.ontotext.com/config/graphdb#disable-sameAs> "true";
-                        <http://www.ontotext.com/config/graphdb#enable-context-index> "false";
-                        <http://www.ontotext.com/config/graphdb#enable-fts-index> "false";
-                        <http://www.ontotext.com/config/graphdb#enable-literal-index> "true";
-                        <http://www.ontotext.com/config/graphdb#enablePredicateList> "true";
-                        <http://www.ontotext.com/config/graphdb#entity-id-size> "32";
-                        <http://www.ontotext.com/config/graphdb#entity-index-size> "10000000";
-                        <http://www.ontotext.com/config/graphdb#fts-indexes> ("default" "iri");
-                        <http://www.ontotext.com/config/graphdb#fts-iris-index> "none";
-                        <http://www.ontotext.com/config/graphdb#fts-string-literals-index> "default";
-                        <http://www.ontotext.com/config/graphdb#imports> "";
-                        <http://www.ontotext.com/config/graphdb#in-memory-literal-properties> "true";
-                        <http://www.ontotext.com/config/graphdb#query-limit-results> "0";
-                        <http://www.ontotext.com/config/graphdb#query-timeout> "0";
-                        <http://www.ontotext.com/config/graphdb#read-only> "false";
-                        <http://www.ontotext.com/config/graphdb#repository-type> "file-repository";
-                        <http://www.ontotext.com/config/graphdb#ruleset> "rdfsplus-optimized";
-                        <http://www.ontotext.com/config/graphdb#storage-folder> "storage";
-                        <http://www.ontotext.com/config/graphdb#throw-QueryEvaluationException-on-timeout> "false";
-                        sail:sailType "graphdb:Sail"
-                    ]
-                ];
-                rdfs:label "" .
+                    <#{username_pattern}> a <http://www.openrdf.org/config/repository#Repository>, config:Repository;
+                        <http://www.openrdf.org/config/repository#repositoryID> "{username_pattern}";
+                        <http://www.openrdf.org/config/repository#repositoryImpl> [
+                              <http://www.openrdf.org/config/repository#repositoryType> "graphdb:SailRepository";
+                              <http://www.openrdf.org/config/repository/sail#sailImpl> [
+                                  <http://rdf4j.org/config/sail/shacl#cacheSelectNodes> true;
+                                  <http://rdf4j.org/config/sail/shacl#dashDataShapes> true;
+                                  <http://rdf4j.org/config/sail/shacl#eclipseRdf4jShaclExtensions> true;
+                                  <http://rdf4j.org/config/sail/shacl#globalLogValidationExecution> false;
+                                  <http://rdf4j.org/config/sail/shacl#logValidationPlans> false;
+                                  <http://rdf4j.org/config/sail/shacl#logValidationViolations> false;
+                                  <http://rdf4j.org/config/sail/shacl#parallelValidation> true;
+                                  <http://rdf4j.org/config/sail/shacl#performanceLogging> false;
+                                  <http://rdf4j.org/config/sail/shacl#rdfsSubClassReasoning> true;
+                                  <http://rdf4j.org/config/sail/shacl#serializableValidation> true;
+                                  <http://rdf4j.org/config/sail/shacl#shapesGraph> <http://rdf4j.org/schema/rdf4j#SHACLShapeGraph>;
+                                  <http://rdf4j.org/config/sail/shacl#transactionalValidationLimit> "500000"^^xsd:long;
+                                  <http://rdf4j.org/config/sail/shacl#validationEnabled> true;
+                                  <http://rdf4j.org/config/sail/shacl#validationResultsLimitPerConstraint> "1000"^^xsd:long;
+                                  <http://rdf4j.org/config/sail/shacl#validationResultsLimitTotal> "1000000"^^xsd:long;
+                                  <http://www.openrdf.org/config/sail#delegate> [
+                                      <http://www.ontotext.com/config/graphdb#base-URL> "http://example.org/owlim#";
+                                      <http://www.ontotext.com/config/graphdb#check-for-inconsistencies> "false";
+                                      <http://www.ontotext.com/config/graphdb#defaultNS> "";
+                                      <http://www.ontotext.com/config/graphdb#disable-sameAs> "true";
+                                      <http://www.ontotext.com/config/graphdb#enable-context-index> "false";
+                                      <http://www.ontotext.com/config/graphdb#enable-fts-index> "false";
+                                      <http://www.ontotext.com/config/graphdb#enable-literal-index> "true";
+                                      <http://www.ontotext.com/config/graphdb#enablePredicateList> "true";
+                                      <http://www.ontotext.com/config/graphdb#entity-id-size> "32";
+                                      <http://www.ontotext.com/config/graphdb#entity-index-size> "10000000";
+                                      <http://www.ontotext.com/config/graphdb#fts-indexes> ("default" "iri");
+                                      <http://www.ontotext.com/config/graphdb#fts-iris-index> "none";
+                                      <http://www.ontotext.com/config/graphdb#fts-string-literals-index> "default";
+                                      <http://www.ontotext.com/config/graphdb#imports> "";
+                                      <http://www.ontotext.com/config/graphdb#in-memory-literal-properties> "true";
+                                      <http://www.ontotext.com/config/graphdb#query-limit-results> "0";
+                                      <http://www.ontotext.com/config/graphdb#query-timeout> "0";
+                                      <http://www.ontotext.com/config/graphdb#read-only> "false";
+                                      <http://www.ontotext.com/config/graphdb#repository-type> "file-repository";
+                                      <http://www.ontotext.com/config/graphdb#ruleset> "rdfsplus-optimized";
+                                      <http://www.ontotext.com/config/graphdb#storage-folder> "storage";
+                                      <http://www.ontotext.com/config/graphdb#throw-QueryEvaluationException-on-timeout>
+                                        "false";
+                                      <http://www.openrdf.org/config/sail#sailType> "graphdb:Sail"
+                                    ];
+                                  <http://www.openrdf.org/config/sail#sailType> "rdf4j:ShaclSail"
+                                ]
+                            ];
+                          rdfs:label "" .
             """
 
         ttl_file = io.BytesIO(ttl_data.encode('utf-8'))
@@ -110,42 +128,60 @@ else:
         if creds == "": continue
         username = creds.split(",")[0]
         ttl_data = f"""
+                    @prefix config: <tag:rdf4j.org,2023:config/> .
                     @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-                    @prefix rep: <http://www.openrdf.org/config/repository#> .
-                    @prefix sr: <http://www.openrdf.org/config/repository/sail#> .
-                    @prefix sail: <http://www.openrdf.org/config/sail#> .
+                    @prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
 
-                    <#{username}> a rep:Repository;
-                        rep:repositoryID "{username}";
-                        rep:repositoryImpl [
-                            rep:repositoryType "graphdb:SailRepository";
-                            <http://www.openrdf.org/config/repository/sail#sailImpl> [
-                                <http://www.ontotext.com/config/graphdb#base-URL> "http://example.org/owlim#";
-                                <http://www.ontotext.com/config/graphdb#check-for-inconsistencies> "false";
-                                <http://www.ontotext.com/config/graphdb#defaultNS> "";
-                                <http://www.ontotext.com/config/graphdb#disable-sameAs> "true";
-                                <http://www.ontotext.com/config/graphdb#enable-context-index> "false";
-                                <http://www.ontotext.com/config/graphdb#enable-fts-index> "false";
-                                <http://www.ontotext.com/config/graphdb#enable-literal-index> "true";
-                                <http://www.ontotext.com/config/graphdb#enablePredicateList> "true";
-                                <http://www.ontotext.com/config/graphdb#entity-id-size> "32";
-                                <http://www.ontotext.com/config/graphdb#entity-index-size> "10000000";
-                                <http://www.ontotext.com/config/graphdb#fts-indexes> ("default" "iri");
-                                <http://www.ontotext.com/config/graphdb#fts-iris-index> "none";
-                                <http://www.ontotext.com/config/graphdb#fts-string-literals-index> "default";
-                                <http://www.ontotext.com/config/graphdb#imports> "";
-                                <http://www.ontotext.com/config/graphdb#in-memory-literal-properties> "true";
-                                <http://www.ontotext.com/config/graphdb#query-limit-results> "0";
-                                <http://www.ontotext.com/config/graphdb#query-timeout> "0";
-                                <http://www.ontotext.com/config/graphdb#read-only> "false";
-                                <http://www.ontotext.com/config/graphdb#repository-type> "file-repository";
-                                <http://www.ontotext.com/config/graphdb#ruleset> "rdfsplus-optimized";
-                                <http://www.ontotext.com/config/graphdb#storage-folder> "storage";
-                                <http://www.ontotext.com/config/graphdb#throw-QueryEvaluationException-on-timeout> "false";
-                                sail:sailType "graphdb:Sail"
-                            ]
-                        ];
-                        rdfs:label "" .
+                    <#{username}> a <http://www.openrdf.org/config/repository#Repository>, config:Repository;
+                        <http://www.openrdf.org/config/repository#repositoryID> "{username}";
+                        <http://www.openrdf.org/config/repository#repositoryImpl> [
+                              <http://www.openrdf.org/config/repository#repositoryType> "graphdb:SailRepository";
+                              <http://www.openrdf.org/config/repository/sail#sailImpl> [
+                                  <http://rdf4j.org/config/sail/shacl#cacheSelectNodes> true;
+                                  <http://rdf4j.org/config/sail/shacl#dashDataShapes> true;
+                                  <http://rdf4j.org/config/sail/shacl#eclipseRdf4jShaclExtensions> true;
+                                  <http://rdf4j.org/config/sail/shacl#globalLogValidationExecution> false;
+                                  <http://rdf4j.org/config/sail/shacl#logValidationPlans> false;
+                                  <http://rdf4j.org/config/sail/shacl#logValidationViolations> false;
+                                  <http://rdf4j.org/config/sail/shacl#parallelValidation> true;
+                                  <http://rdf4j.org/config/sail/shacl#performanceLogging> false;
+                                  <http://rdf4j.org/config/sail/shacl#rdfsSubClassReasoning> true;
+                                  <http://rdf4j.org/config/sail/shacl#serializableValidation> true;
+                                  <http://rdf4j.org/config/sail/shacl#shapesGraph> <http://rdf4j.org/schema/rdf4j#SHACLShapeGraph>;
+                                  <http://rdf4j.org/config/sail/shacl#transactionalValidationLimit> "500000"^^xsd:long;
+                                  <http://rdf4j.org/config/sail/shacl#validationEnabled> true;
+                                  <http://rdf4j.org/config/sail/shacl#validationResultsLimitPerConstraint> "1000"^^xsd:long;
+                                  <http://rdf4j.org/config/sail/shacl#validationResultsLimitTotal> "1000000"^^xsd:long;
+                                  <http://www.openrdf.org/config/sail#delegate> [
+                                      <http://www.ontotext.com/config/graphdb#base-URL> "http://example.org/owlim#";
+                                      <http://www.ontotext.com/config/graphdb#check-for-inconsistencies> "false";
+                                      <http://www.ontotext.com/config/graphdb#defaultNS> "";
+                                      <http://www.ontotext.com/config/graphdb#disable-sameAs> "true";
+                                      <http://www.ontotext.com/config/graphdb#enable-context-index> "false";
+                                      <http://www.ontotext.com/config/graphdb#enable-fts-index> "false";
+                                      <http://www.ontotext.com/config/graphdb#enable-literal-index> "true";
+                                      <http://www.ontotext.com/config/graphdb#enablePredicateList> "true";
+                                      <http://www.ontotext.com/config/graphdb#entity-id-size> "32";
+                                      <http://www.ontotext.com/config/graphdb#entity-index-size> "10000000";
+                                      <http://www.ontotext.com/config/graphdb#fts-indexes> ("default" "iri");
+                                      <http://www.ontotext.com/config/graphdb#fts-iris-index> "none";
+                                      <http://www.ontotext.com/config/graphdb#fts-string-literals-index> "default";
+                                      <http://www.ontotext.com/config/graphdb#imports> "";
+                                      <http://www.ontotext.com/config/graphdb#in-memory-literal-properties> "true";
+                                      <http://www.ontotext.com/config/graphdb#query-limit-results> "0";
+                                      <http://www.ontotext.com/config/graphdb#query-timeout> "0";
+                                      <http://www.ontotext.com/config/graphdb#read-only> "false";
+                                      <http://www.ontotext.com/config/graphdb#repository-type> "file-repository";
+                                      <http://www.ontotext.com/config/graphdb#ruleset> "rdfsplus-optimized";
+                                      <http://www.ontotext.com/config/graphdb#storage-folder> "storage";
+                                      <http://www.ontotext.com/config/graphdb#throw-QueryEvaluationException-on-timeout>
+                                        "false";
+                                      <http://www.openrdf.org/config/sail#sailType> "graphdb:Sail"
+                                    ];
+                                  <http://www.openrdf.org/config/sail#sailType> "rdf4j:ShaclSail"
+                                ]
+                            ];
+                          rdfs:label "" .
                     """
 
         ttl_file = io.BytesIO(ttl_data.encode('utf-8'))
