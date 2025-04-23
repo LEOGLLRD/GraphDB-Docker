c7f215e6e0e94cb4a5cf978ecb01ef711f8c5293
LEOGLLRD <92512809+LEOGLLRD@users.noreply.github.com>
2025-04-23 16:15:24 +0200

    Added the possibility to give argument

--- graphdb-2-services.sh
+++ graphdb-2-services.sh
@@ -34,22 +34,32 @@ python_version=$(echo "$versions" | cut -d '_' -f2)
 
 # Creating the folder containing all the required files and folders to create the stack
 mkdir -p ./LEOGLLRD-graphdb
-
-curl -L -o ./LEOGLLRD/graphdb.zip https://github.com/LEOGLLRD/GraphDB-Docker/archive/refs/heads/2-services-dockercompose.zip
+branch="several-services-dockercompose"
+project="GraphDB-Docker"
+curl -L -o ./LEOGLLRD-graphdb/graphdb.zip https://github.com/LEOGLLRD/GraphDB-Docker/archive/refs/heads/several-services-dockercompose.zip
 unzip ./LEOGLLRD-graphdb/graphdb.zip -d ./LEOGLLRD-graphdb
 rm ./LEOGLLRD-graphdb/graphdb.zip
 echo "Everything required is downloaded."
+
+# Modifying the env vars
+ENV_FILE="./LEOGLLRD-graphdb/$project-$branch/vars.env"
+VAR="NUMBER_OF_USERS"
+VALUE=$number_of_users
+sed -i "s|^$VAR=.*|$VAR=$VALUE|" "$ENV_FILE"
+VAR="NEW_USERS_PATTERN"
+VALUE=$new_users_pattern
+sed -i "s|^$VAR=.*|$VAR=$VALUE|" "$ENV_FILE"
 
 echo "Setting up the docker environment ..."
 docker volume create "shared-volume"
 echo "Volume created !"
 echo "Copying the required files to the volume by using a temporary container ..."
 docker container create --name temporary -v shared-volume:/shared-volume busybox
-docker cp ./generated/graphdb-project temporary:/shared-volume/
+docker cp ./LEOGLLRD-graphdb/$project-$branch/graphdb-project temporary:/shared-volume/
 echo "Files copied !"
 docker rm temporary
 echo "Temporary container deleted !"
 echo "Running docker compose up ..."
 
 
-docker compose -f ./LEOGLLRD-graphdb/GraphDB-Docker-2-services-dockercompose/docker-compose.yml up
+docker compose -f "./LEOGLLRD-graphdb/$project-$branch/docker-compose.yml" up
